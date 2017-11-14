6ccfce47da5785a2a4209849f6a198fe477ed428
andrewchambers <andrewchamberss@gmail.com>
2017-11-14 19:55:25 +1300

    Update gate.sh

--- gate.sh
+++ gate.sh
@@ -4,8 +4,10 @@ set -u
 
 export WEBSOCKETEMAIL_TOKEN=$(cat websocketemail_token.txt)
 
+# A secret id that people can't guess
 gateid=gate12345
 
+# initialize gpio
 echo 13 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio13/direction
 
@@ -14,6 +16,7 @@ do
   if wsemail -timeout 0 -for-address $gateid@websocket.email \
      | grep -q "open sesame"
   then
+    # Trigger the gate if the email contained the secret incantation
     echo 1 > /sys/class/gpio/gpio13/value 
     sleep 0.1
     echo 0 > /sys/class/gpio/gpio13/value 
@@ -21,4 +24,3 @@ do
   # don't loop too fast if there is an error somewhere.
   sleep 1
 done
-
